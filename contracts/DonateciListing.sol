fe41b7284765460e913720d36f2ab712d2f03fb4
tanisman <mail@sezercantanisman.com.tr>
2021-12-18 01:11:33 +0300

    donateci listing & nft contracts

---
+++ contracts/DonateciListing.sol
@@ -0,0 +1,93 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./interfaces/IWBITCI.sol";
+import "./Donateci.sol";
+import "./DonateciNFT.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DonateciListing {
+    using Counters for Counters.Counter;
+    struct NFTListingInfo {
+        address creatorAddress;
+        uint256 tokenId;
+        uint256 priceInWeiDNC;
+        bool sold;
+    }
+
+    Counters.Counter private _creatorIds;
+    Counters.Counter private _nftListingIds;
+    mapping(address => bool) private _isCreator; 
+    mapping(uint256 => address) private _creators;
+    mapping(uint256 => NFTListingInfo) private _nftListings;
+
+    uint256 private _creatorBurnFee = 100; //without decimals
+    address public immutable DNC;
+    address public immutable DNFT;
+
+    constructor(address _dnc, address _dnft) {
+        DNC = _dnc;
+        DNFT = _dnft;
+    }
+
+    function becomeCreator() public returns (bool) {
+        require(!_isCreator[msg.sender]);
+        Donateci dncContract = Donateci(DNC);
+        require(dncContract.transfer(address(0x0), _creatorBurnFee * 10 ** dncContract.decimals()));
+                
+        _creatorIds.increment();
+        uint256 newId = _creatorIds.current();
+
+        _isCreator[msg.sender] = true;
+        _creators[newId] = msg.sender;
+
+        return true;
+    }
+
+    function getCreatorCount() public view returns (uint256) {
+        return _creatorIds.current();
+    }
+
+    function getCreatorAt(uint256 _idx) public view returns (address) {
+        return _creators[_idx];
+    }
+
+    function isCreator(address _addr) public view returns (bool) {
+        return _isCreator[_addr];
+    }
+
+    function addNFTListing(uint256 _tokenId, uint256 _desiredPriceInWeiDNC) public returns (bool) {
+        //change NFT ownership to this contract
+        DonateciNFT(DNFT).transferFrom(msg.sender, address(this), _tokenId); //now contract owns the NFT
+        
+        _nftListingIds.increment();
+        uint256 newId = _nftListingIds.current();
+        //add new listing
+        _nftListings[newId] = NFTListingInfo({
+            creatorAddress: msg.sender, tokenId: _tokenId, 
+            priceInWeiDNC: _desiredPriceInWeiDNC, sold: false});  
+        
+        return true;
+    }
+
+    function getNFTListingCount() public view returns (uint256) {
+        return _nftListingIds.current();
+    }
+
+    function getNFTListingAt(uint256 _idx) public view returns (address creator, uint256 tokenId, uint256 priceInWeiDNC) {
+        NFTListingInfo storage listing = _nftListings[_idx];
+        if (listing.sold) { //if already sold
+            return (address(0x0), 0, 0); //return invalid
+        } else {
+            return (listing.creatorAddress, listing.tokenId, listing.priceInWeiDNC);
+        }
+    }
+
+    function buyNFT(uint256 _idx) public returns (bool) {
+        NFTListingInfo storage listing = _nftListings[_idx];
+        require(!listing.sold);  //validate if not sold
+        require(Donateci(DNC).transfer(listing.creatorAddress, listing.priceInWeiDNC)); //pay to creator (we can get commission here)
+        DonateciNFT(DNFT).transferFrom(address(this), msg.sender, listing.tokenId); //transfer ownership of NFT
+
+        return true;
+    }
+}
---
+++ contracts/DonateciNFT.sol
@@ -0,0 +1,59 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract DonateciNFT is ERC721, ERC721Enumerable, ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("DonateciNFT", "DNFT") {
+
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+      internal
+      override(ERC721, ERC721Enumerable)
+  {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+      public
+      view
+      override(ERC721, ERC721URIStorage)
+      returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+      public
+      view
+      override(ERC721, ERC721Enumerable)
+      returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
+
+  function _baseURI() internal pure override returns (string memory) {
+    return "";
+  }
+
+
+  function mintDNFT(address owner, string memory tURI) public returns (uint256) {
+    _tokenIds.increment();
+
+    uint256 newId = _tokenIds.current();
+    _mint(owner, newId); //mint new nft on this ERC721
+    _setTokenURI(newId, tURI); //set uri (e.g. ipfs uri)
+    return newId;
+  }
+}
